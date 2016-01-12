17c87dc9f2e8732f64a7c561e0e24db665fc2952
Imran <mihussain@hotmail.co.uk>
2016-01-12 23:16:55 +0000

    pull out images and link url

--- single-project.php
+++ single-project.php
@@ -16,9 +16,53 @@
 
 			<article>
 
-				<h2><?php the_title(); ?> TEST</h2>
+				<h2><?php the_title(); ?></h2>
 				<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-				<?php the_content(); ?>			
+				<?php the_content(); ?>		
+
+				<a href="<?php the_field('url'); ?>" target="_blank">View Project</a> 	
+
+				<?php
+				if( have_rows('images') ):
+
+				    while ( have_rows('images') ) : the_row(); 
+
+						$image = get_sub_field('image');
+						if( !empty($image) ): 
+
+							// vars
+							$url = $image['url'];
+							$title = $image['title'];
+							$alt = $image['alt'];
+							$caption = $image['caption'];
+
+							// thumbnail
+							$size = 'thumbnail';
+							$thumb = $image['sizes'][ $size ];
+							$width = $image['sizes'][ $size . '-width' ];
+							$height = $image['sizes'][ $size . '-height' ];
+
+							if( $caption ): ?>
+								<div class="wp-caption">
+							<?php endif; ?>
+
+							<a href="<?php echo $url; ?>" title="<?php echo $title; ?>">
+								<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
+							</a>
+
+							<?php if( $caption ): ?>
+									<p class="wp-caption-text"><?php echo $caption; ?></p>
+								</div>
+
+							<?php endif; ?>
+						<?php endif; ?>
+				<?php
+				    endwhile;
+
+				else :
+				    // no rows found
+				endif;
+				?>
 
 				<?php if ( get_the_author_meta( 'description' ) ) : ?>
 				<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
