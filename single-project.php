098c0b76c0823da06d168e46ccd8b63c0b89cb72
Imran <mihussain@hotmail.co.uk>
2016-01-13 22:42:02 +0000

    Add featured image to single page post

--- single-project.php
+++ single-project.php
@@ -10,11 +10,20 @@
  */
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+		<?php if (has_post_thumbnail( $post->ID ) ): ?>
+			<?php $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+		  	<img class="featured-image" src="<?php echo $featuredImage[0]; ?>" />
+		<?php endif; ?>
+
 	<section class="page-content">
 		<div class="central_container"> 
-			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+			
 
 			<article>
+
+				
 
 				<h2><?php the_title(); ?></h2>
 				<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
